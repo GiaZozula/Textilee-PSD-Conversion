549d2fd2f0f9f12c9f3e92d6682224293910ecaa
olstinker <giazozula@gmail.com>
2022-10-30 21:19:58 -0400

    finished assignment two, extremely basic version

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // ASSIGNMENT TWO JAVASCRIPT
 
+// attempting to follow the code-along by adding some js functionality to the submission of the form, in this case on the contact page
 
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const h2El= document.querySelector("h2");
+    h2El.innerHTML = "YOU DID ITTTT";
+
+    console.log("thank u for yr comment");
+});
